6ccdd0e4c67708fb64acc4c067d553da68e536f8
abbycakes02 <114423578+abbycakes02@users.noreply.github.com>
2023-09-19 09:58:38 -0700

    final configuration hopefully

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//  plugins: [react()],
+//})
+
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    vite: {
+        server: {
+            watch: {
+                usePolling: true,
+            },
+        },
+    },
+});
+
